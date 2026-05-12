da17ee8dc3f1e8a90315f3fd755f92f327d8e3be
sharonjoseph0909 <shamashama2016@gmail.com>
2026-05-11 22:12:20 -0400

    first project push

---
+++ webSearch.py
@@ -0,0 +1,6 @@
+document = {
+    1: "Infinity Train is a TV show for tweens",
+    2: "Roblox is a multi-player game",
+    3: "Girls are emotionally intelligent and have a tendency to mature faster",
+    4: "Tampa is a city in Florida."
+}
